8b596659bbebf1b5047cdeaa70cc8851bf95ef84
G0dObject <vladimirgjob@gmail.com>
2022-12-01 17:32:54 +0300

    u

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://176.213.142.58:443/";
+export const API_URL = "https://176.213.142.58:443/";
 
 let $api = axios.create({
 	baseUrl: API_URL,
